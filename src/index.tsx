da6cbae92f43989d0c8aade87c54f24b7b2fa80f
atanackov12 <djordje_atanackov@uns.ac.rs>
2023-07-08 22:15:19 +0200

    commit frontend

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './app/layout/styles.css';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/router/Router';
+import { Provider } from 'react-redux';
+import { store } from './app/store/configureStore';
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+    <RouterProvider router={router}/>
+    </Provider>
   </React.StrictMode>
 );
 
